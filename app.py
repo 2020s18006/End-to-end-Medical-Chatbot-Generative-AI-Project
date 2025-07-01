ce84ebdfbf26b889dede3d5e5a6b4d9bd9be9c68
2020s18006 <maslambrantha@gmail.com>
2025-07-01 07:19:37 -0700

    All the packages are updated

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template, jsonify, request
 from src.helper import download_hugging_face_embeddings
 from langchain_pinecone import PineconeVectorStore
-from langchain_openai import OpenAI
+from langchain_openai import ChatOpenAI
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
 from src.prompt import *
 import os
+
+os.environ.pop("OPENAI_API_KEY", None)
 
 app = Flask(__name__)
 
@@ -16,11 +18,11 @@ load_dotenv()
 PINECONE_API_KEY=os.environ.get('PINECONE_API_KEY')
 OPENAI_API_KEY=os.environ.get('OPENAI_API_KEY')
 
-os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
-os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+print("OpenAI Key:", OPENAI_API_KEY)
+print("Pinecone Key:", PINECONE_API_KEY)
+
 
 embeddings = download_hugging_face_embeddings()
-
 
 index_name = "medicalbot"
 
@@ -32,8 +34,7 @@ docsearch = PineconeVectorStore.from_existing_index(
 
 retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k":3})
 
-
-llm = OpenAI(temperature=0.4, max_tokens=500)
+llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.4, max_tokens=500)
 prompt = ChatPromptTemplate.from_messages(
     [
         ("system", system_prompt),
@@ -44,11 +45,9 @@ prompt = ChatPromptTemplate.from_messages(
 question_answer_chain = create_stuff_documents_chain(llm, prompt)
 rag_chain = create_retrieval_chain(retriever, question_answer_chain)
 
-
 @app.route("/")
 def index():
     return render_template('chat.html')
-
 
 @app.route("/get", methods=["GET", "POST"])
 def chat():
@@ -59,8 +58,5 @@ def chat():
     print("Response : ", response["answer"])
     return str(response["answer"])
 
-
-
-
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port= 8080, debug= True)
+    app.run(host="0.0.0.0", port=8080, debug=True, use_reloader=False)
